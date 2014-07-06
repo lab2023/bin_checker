8dd67fd393abd7728083b4f5896ac89ffd866e56
Onur Ozgur OZKAN <onur.ozgur.ozkan@lab2023.com>
2014-07-06 23:54:32 +0300

    Add logger for debug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,13 @@ require 'rubygems'
 require 'bundler/setup'
 require 'active_model'
 require 'active_record'
+require 'logger'
 
 Bundler.setup
 
 require 'bin_checker'
+
+ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "../log/debug.log"))
 
 RSpec.configure do |config|
 
